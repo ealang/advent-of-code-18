c155d838b4326e8963ee189aeeceb37dca0d5e1d
Eric Lang <ealang@gmail.com>
2018-12-02 14:11:37 -0600

    day 1

---
+++ 01/day01.hs
@@ -0,0 +1,20 @@
+import qualified Data.Set as Set
+
+part1 :: [Int] -> Int
+part1 = sum
+
+flatten :: [[Int]] -> [Int]
+flatten nested = [ elem | list <- nested, elem <- list ]
+
+part2 :: [Int] -> Int
+part2 freqs = freqs' Set.empty stream
+  where stream = scanl (+) 0 (flatten . repeat $ freqs)
+        freqs' seen (v:xs) = if Set.member v seen
+                             then v
+                             else freqs' (Set.insert v seen) xs
+
+main = do
+  let parseInt str = read (dropWhile (=='+') str) :: Int
+  freqs <- map parseInt . lines <$> readFile "day01.txt"
+  print $ part1 freqs
+  print $ part2 freqs
