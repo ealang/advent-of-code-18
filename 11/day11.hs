2be19f65824290a6d864d571ccd13c97ae682c66
elang2 <ealang@gmail.com>
2018-12-13 22:58:07 -0600

    day 11 part 2

--- 11/day11.hs
+++ 11/day11.hs
@@ -1,7 +1,7 @@
+import Control.Monad (forM_)
 import Data.Vector ((!), Vector)
 import qualified Data.Vector as Vector
 import qualified Data.Vector.Mutable as MV
-import Control.Monad (forM_)
 
 type Pt = (Int, Int)
 
@@ -14,11 +14,11 @@ walkGrid :: Int -> [Pt]
 walkGrid n = [ (x, y) | y <- [0..n - 1],
                         x <- [0..n - 1] ]
 
-gridSum :: Vector Int -> Int -> Pt -> Int -> Int
-gridSum sumTable n (x, y) size = val - top - left + diag
+gridSumFromTable :: Vector Int -> Int -> Pt -> Int -> Int
+gridSumFromTable sumTable n (x, y) size = val - top - left + diag
     where i = (x - 1) + (y - 1) * n
           diag = if x > 0 && y > 0 then sumTable ! i              else 0
-          top =  if y > 0          then sumTable ! (i + size)     else 0
+          top  = if y > 0          then sumTable ! (i + size)     else 0
           left = if x > 0          then sumTable ! (i + size * n) else 0
           val = sumTable ! (i + size + n * size)
 
@@ -33,21 +33,27 @@ genSumTable valueAt n = do cache <- MV.new (n * n)
           left <- if x > 0          then MV.read cache (i - 1)     else return 0
           MV.write cache i (top + left - diag + valueAt (x, y))
 
-fuelGridWalk3x3 :: Vector Int -> Int -> [(Int, Pt)]
-fuelGridWalk3x3 sumTable n = map withValues pts
-  where pts = walkGrid (n - 2)
-        withValues pt = (gridSum sumTable n pt 3, pt)
+fuelGridWalkMxM :: (Pt -> Int -> Int) -> Int -> Int -> [(Int, Pt)]
+fuelGridWalkMxM gridSum n m = map withValues pts
+  where pts = walkGrid (n - (m - 1))
+        withValues pt = (gridSum pt m, pt)
 
-part1 :: Vector Int -> Int -> Pt
-part1 sumTable n = snd . maximum $ fuelGridWalk3x3 sumTable n
+part1 :: (Pt -> Int -> Int) -> Int -> Pt
+part1 gridSum n = format . snd . maximum $ fuelGridWalkMxM gridSum n 3
+  where format (x, y) = (x + 1, y + 1)
 
+part2 :: (Pt -> Int -> Int) -> Int -> (Pt, Int)
+part2 gridSum n = format . maximum $ [ (val, pt, m) | 
+                                          m <- [1..n],
+                                          (val, pt) <- fuelGridWalkMxM gridSum n m ]
+  where format (_, (x, y), m) = ((x + 1, y + 1), m)
 
 main = do
   let serial = 8199
   let n = 300
 
   sumTable <- genSumTable (cellValue serial) n
+  let gridSum = gridSumFromTable sumTable n
 
-  let add (x, y) = (x + 1, y + 1)
-  print $ add (part1 sumTable n) -- 235,87
-
+  print $ part1 gridSum n
+  print $ part2 gridSum n
